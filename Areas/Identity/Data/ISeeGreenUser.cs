b98e98759a8a23907006dce832d90eb4a76d3a35
reidst <reidst@hendrix.edu>
2023-12-01 12:12:47 -0600

    Scaffolded Identity

---
+++ Areas/Identity/Data/ISeeGreenUser.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace csci340_iseegreen.Areas.Identity.Data;
+
+public class ISeeGreenUser : IdentityUser
+{
+}
--- Data/ISeeGreenContext.cs
+++ Data/ISeeGreenContext.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using csci340_iseegreen.Areas.Identity.Data;
 
 namespace csci340_iseegreen.Data
 {
-    public class ISeeGreenContext : DbContext
+    public class ISeeGreenContext : IdentityDbContext<ISeeGreenUser>
     {
         public ISeeGreenContext(DbContextOptions<ISeeGreenContext> options)
             : base(options)
@@ -16,12 +19,14 @@ namespace csci340_iseegreen.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<csci340_iseegreen.Models.Categories>().ToTable("Categories");
             modelBuilder.Entity<csci340_iseegreen.Models.TaxonomicOrders>().ToTable("TaxonomicOrders");
             modelBuilder.Entity<csci340_iseegreen.Models.Families>().ToTable("Families");
             modelBuilder.Entity<csci340_iseegreen.Models.Genera>().ToTable("Genera");
             modelBuilder.Entity<csci340_iseegreen.Models.Taxa>().ToTable("Taxa");
             modelBuilder.Entity<csci340_iseegreen.Models.Synonyms>().ToTable("Synonyms");
+            modelBuilder.Entity<csci340_iseegreen.Areas.Identity.Data.ISeeGreenUser>().ToTable("ISeeGreenUsers");
         }
 
         public DbSet<csci340_iseegreen.Models.Categories> Categories { get; set; } = default!;
@@ -30,5 +35,6 @@ namespace csci340_iseegreen.Data
         public DbSet<csci340_iseegreen.Models.Genera> Genera { get; set; } = default!;
         public DbSet<csci340_iseegreen.Models.Taxa> Taxa { get; set; } = default!;
         public DbSet<csci340_iseegreen.Models.Synonyms> Synonyms { get; set; } = default!;
+        public DbSet<csci340_iseegreen.Areas.Identity.Data.ISeeGreenUser> ISeeGreenUsers { get; set; } = default!;
     }
 }
